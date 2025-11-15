0eb2e1e1812fec1c7c000b14ee38033efcee42a9
SiphiweR-Sage <siphiwe.radebe@sage.com>
2025-11-15 18:18:02 +0200

    dev

---
+++ WeatherApp.Core/Exceptions/BusinessException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WeatherApp.Core.Exceptions
+{
+    public class BusinessException : Exception
+    {
+        public BusinessException(string message) : base(message)
+        {
+        }
+
+        public BusinessException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
